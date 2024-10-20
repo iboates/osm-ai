f872069f6bcb06c04ef01ce01b54a3416a109705
isaac <iboates@gmail.com>
2024-10-20 14:05:28 +0200

    done for the hackathon

--- hackathon.py
+++ hackathon.py
@@ -1,4 +1,3 @@
-from glob import glob
 import json
 from datetime import datetime
 
@@ -6,31 +5,33 @@ from dotenv import load_dotenv
 import sqlalchemy as sa
 import geoalchemy2   # NEED THIS OTHERWISE GEOMETRY COLUMNS ARE NOT "SEEN" BY SQLALCHEMY WHEN DESCRIBING TABLES
 
-from langchain_openai import ChatOpenAI, OpenAI
-from langchain.agents import tool, AgentExecutor
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from langchain.agents import AgentExecutor
 from langchain_core.prompts import PromptTemplate
-from langchain.agents.format_scratchpad.openai_tools import (
-    format_to_openai_tool_messages,
-)
-from langchain.agents.output_parsers.openai_tools import OpenAIToolsAgentOutputParser
 from langchain_core.messages import HumanMessage
-from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_community.utilities import SQLDatabase
 from langchain_experimental.sql import SQLDatabaseChain
 from langchain_community.agent_toolkits.sql.base import create_sql_agent
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain.agents.agent_types import AgentType
-from langchain.chains import APIChain
-from langchain_openai import OpenAI
-from langchain_core.runnables import RunnablePassthrough
 
 import osmai
 
 load_dotenv()
 
-#CHATGPT_VERSION="gpt-4o"
-CHATGPT_VERSION="gpt-3.5-turbo"
+
+# other shit to try
+# 1. pre-made list of top X tag values for important tags to help query definition
+# 2. post-processing of final query to analyze for performance concerns (potentially with EXPLAIN)
+# 3. agent for interacting with nominatim for named places
+
+
+
+# NOTE: It seems that 3.5-turbo is not smart enough to fully understand all the prompts.
+# Switching to 4o makes it a lot more aware of important rules like respecting desired CRS transformations
+
+CHATGPT_VERSION="gpt-4o"
+#CHATGPT_VERSION="gpt-3.5-turbo"
 STOP_PATTERN = "$$STOP$$"
 COUNTRY = "andorra"
 COUNTRY_DB_PORT = {"andorra": 5433,
@@ -46,18 +47,6 @@ db = SQLDatabase(engine,
                  include_tables=["planet_osm_point", "planet_osm_line", "planet_osm_polygon"],
                  custom_table_info=table_descriptions)
 prompt = PromptTemplate.from_template(open("osmai/prompts/featureset.txt").read())
-
-# # taginfo API
-# chain = APIChain.from_llm_and_api_docs(
-#     llm,
-#     open("osmai/prompts/taginfo_docs_plaintext.txt").read(),
-#     verbose=True,
-#     limit_to_domains=["https://taginfo.openstreetmap.org/taginfo/"],
-# )
-# chain.run(
-#     "Get the top 10 values for the 'natural' key"
-# )
-
 
 sample_inputs = open("sample_inputs/task_decomposer.txt").readlines()
 
@@ -115,14 +104,22 @@ for sample_input in sample_inputs:
         max_iterations=10,
         agent_executor_kwargs={"handle_parsing_errors": True})
     generated_sql = final_query_agent_executor.invoke({"input": json.dumps(final_query_inputs)})
-    final_query = generated_sql["output"]
+    final_query = generated_sql["output"].strip()
 
     formatted_date = datetime.now().strftime("%Y%m%d%H%M%S")
     with engine.connect() as conn:
         transaction = conn.begin()
 
-        if final_query.strip()[-1] == ";":
-            final_query = final_query.strip()[:-1]
+        if final_query[:6] == "```sql":
+            final_query = final_query[6:]
+
+        if final_query[-3:] == "```":
+            final_query = final_query[:-3]
+
+        final_query = final_query.strip()
+
+        if final_query[-1] == ";":
+            final_query = final_query[:-1]
 
         create_query = f"""
         
